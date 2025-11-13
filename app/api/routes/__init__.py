00ae08d948c3651a8e3d85eeba7f68821f0d4eff
honglue.mhl <honglue.mhl@gongdao.com>
2025-11-13 19:23:49 +0800

    feat: 创建样本表

--- app/api/routes/__init__.py
+++ app/api/routes/__init__.py
@@ -2,9 +2,15 @@ from fastapi import APIRouter
 
 from .users import router as users_router
 from .wx import router as wx_router
+from .projects import router as projects_router
+from .organization import router as organization_router
+from .sample import router as sample_router
 
 
 api_router = APIRouter()
 
 api_router.include_router(users_router, prefix="/users", tags=["users"])
 api_router.include_router(wx_router, prefix="/wx", tags=["wx"])
+api_router.include_router(projects_router, prefix="/projects", tags=["projects"])
+api_router.include_router(organization_router, prefix="/organizations", tags=["organizations"])
+api_router.include_router(sample_router, prefix="/samples", tags=["samples"])
