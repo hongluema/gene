200e793ab485ec8e4d062e541ca58d748119450c
honglue.mhl <honglue.mhl@gongdao.com>
2025-11-17 11:42:09 +0800

    feat: 和外部服务器交互

--- app/api/routes/__init__.py
+++ app/api/routes/__init__.py
@@ -4,6 +4,7 @@ from .users import router as users_router
 from .projects import router as projects_router
 from .organization import router as organization_router
 from .sample import router as sample_router
+from .remote import router as remote_router
 
 
 api_router = APIRouter()
@@ -12,3 +13,4 @@ api_router.include_router(users_router, prefix="/users", tags=["users"])
 api_router.include_router(projects_router, prefix="/projects", tags=["projects"])
 api_router.include_router(organization_router, prefix="/organizations", tags=["organizations"])
 api_router.include_router(sample_router, prefix="/samples", tags=["samples"])
+api_router.include_router(remote_router, tags=["remote"])  # exposes /api/token
