d999c67c444db322c3288aaa21c659fa070720d8
honglue.mhl <honglue.mhl@gongdao.com>
2025-11-25 11:24:45 +0800

    feat: 改为内网环境

--- app/db/base.py
+++ app/db/base.py
@@ -7,9 +7,9 @@ from sqlalchemy.orm import sessionmaker
 
 # --- 数据库 1: Applet WY ---
 # TODO：正式环境
-# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://walry:arApp123qwe..@10.110.1.22:33060/applet_wy?charset=utf8mb4"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://walry:arApp123qwe..@10.110.1.22:33060/applet_wy?charset=utf8mb4"
 # TODO：开发环境
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://walry:arApp123qwe..@117.149.9.79:33060/applet_wy?charset=utf8mb4"
+# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://walry:arApp123qwe..@117.149.9.79:33060/applet_wy?charset=utf8mb4"
 # 创建数据库引擎（echo=True 会打印 SQL 语句，调试用）
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
@@ -29,7 +29,10 @@ Base = declarative_base()
 
 # --- 数据库 2: LIMS ---
 # 注意这里将数据库名改为了 lims
-SQLALCHEMY_DATABASE_URL_LIMS = "mysql+pymysql://walry:arApp123qwe..@117.149.9.79:33060/lims?charset=utf8mb4"
+# TODO：正式环境
+SQLALCHEMY_DATABASE_URL_LIMS = "mysql+pymysql://walry:arApp123qwe..@10.110.1.22:33060/lims?charset=utf8mb4"
+# TODO：开发环境
+# SQLALCHEMY_DATABASE_URL_LIMS = "mysql+pymysql://walry:arApp123qwe..@117.149.9.79:33060/lims?charset=utf8mb4"
 
 engine_lims = create_engine(
     SQLALCHEMY_DATABASE_URL_LIMS, 
