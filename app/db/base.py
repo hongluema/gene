15f3c08785859e6c51b56ccf2cb6718bc427627b
阿坤 <46715095+CaptionLittleBird@users.noreply.github.com>
2025-12-02 11:57:11 +0800

    修改开发环境url

--- app/db/base.py
+++ app/db/base.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from settings import BASE_IP
 
 # MySQL 连接 URL 格式：
 # mysql+pymysql://用户名:密码@主机地址:端口号/数据库名?charset=utf8mb4
@@ -9,7 +10,7 @@ from sqlalchemy.orm import sessionmaker
 # TODO：正式环境
 # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://walry:arApp123qwe..@10.110.1.22:33060/applet_wy?charset=utf8mb4"
 # TODO：开发环境
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://walry:arApp123qwe..@117.149.9.79:33060/applet_wy?charset=utf8mb4"
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://walry:arApp123qwe..@{BASE_IP}:33060/applet_wy?charset=utf8mb4"
 # 创建数据库引擎（echo=True 会打印 SQL 语句，调试用）
 # 注意：pymysql 在处理大整数时可能会丢失精度，需要在连接时设置参数
 engine = create_engine(
@@ -33,7 +34,7 @@ Base = declarative_base()
 # TODO：正式环境
 # SQLALCHEMY_DATABASE_URL_LIMS = "mysql+pymysql://walry:arApp123qwe..@10.110.1.22:33060/lims?charset=utf8mb4"
 # TODO：开发环境
-SQLALCHEMY_DATABASE_URL_LIMS = "mysql+pymysql://walry:arApp123qwe..@117.149.9.79:33060/lims?charset=utf8mb4"
+SQLALCHEMY_DATABASE_URL_LIMS = f"mysql+pymysql://walry:arApp123qwe..@{BASE_IP}:33060/lims?charset=utf8mb4"
 
 engine_lims = create_engine(
     SQLALCHEMY_DATABASE_URL_LIMS, 
